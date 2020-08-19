ea5dd745c5ad851a786abae2a911e429b5a66e8d
18088665889 <1291752767@qq.com>
2020-08-19 16:44:59 +0800

    哈赛

--- gitstudy/src/com/neuedu/day01/Hello.java
+++ gitstudy/src/com/neuedu/day01/Hello.java
@@ -5,11 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello1");
 		System.out.println("hello2");
-		System.out.println("hello3");
-		System.out.println("hello4");
-
-		System.out.println("hello5");
-		System.out.println("===============================");
-		System.out.println("hello1233");
+		
 	}
 }
