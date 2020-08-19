2a5fbb4e9895e6e8ac4ef4ad0a4ecbe5b6f258a7
18842613684 <69197413+18842613684@users.noreply.github.com>
2020-08-19 16:55:43 +0800

    Update Hello.java
    kwdy

--- gitstudy/src/com/neuedu/day01/Hello.java
+++ gitstudy/src/com/neuedu/day01/Hello.java
@@ -3,9 +3,9 @@ package com.neuedu.day01;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("hello1");
-		System.out.println("hello2");
-		System.out.println("hello3");
+		System.out.println("404");
+		
+		
 		
 	}
 }
